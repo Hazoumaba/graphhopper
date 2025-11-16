48de40a2a2a4d771566a479852eb605779139ba6
Hazem Ben Amor <69153224+Hazoumaba@users.noreply.github.com>
2025-11-16 15:37:41 -0500

    test regression

---
+++ core/src/main/java/com/graphhopper/util/UntestedCalculator.java
@@ -0,0 +1,15 @@
+package com.graphhopper.util;
+
+public class UntestedCalculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+}
