00bf98a0d4aff7edbabf144957d55dec360245a0
Hazem Ben Amor <69153224+Hazoumaba@users.noreply.github.com>
2025-11-16 23:48:24 -0500

    Create untestedCalculator.java

---
+++ core/src/main/java/com/graphhopper/util/untestedCalculator.java
@@ -0,0 +1,15 @@
+package com.graphhopper.util;
+
+public class untestedCalculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+}
